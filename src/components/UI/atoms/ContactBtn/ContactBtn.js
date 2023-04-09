19a1b18b76a6128008e10d0f003c979dc003203a
katesereda1808 <katesereda22@gmail.com>
2023-04-09 15:22:19 +0200

    feat: add controlled inputs, add checkbox to the form, add button disabled state

--- src/components/UI/atoms/ContactBtn/ContactBtn.js
+++ src/components/UI/atoms/ContactBtn/ContactBtn.js
@@ -1,9 +1,13 @@
 import styles from "./ContactBtn.module.css";
 
-const ContactBtn = ({ text, btnColor }) => {
+const ContactBtn = ({ text, btnColor, disabledMode }) => {
   return (
     <button
-      className={`${styles.btn} ${btnColor === "white_btn" && styles.white_btn}`}
+      className={`${styles.btn} ${
+        btnColor === "white_btn" && styles.white_btn
+      }`}
+      // {disabledMode?'disabled':''}
+      disabled={disabledMode}
     >
       {text}
     </button>
--- src/components/UI/molecules/Form/Form.js
+++ src/components/UI/molecules/Form/Form.js
@@ -12,10 +12,26 @@ const Form = ({ btnText, btnColor, options, selectPlaceholder }) => {
     e.preventDefault();
     console.log(office);
   };
-  // валидация
-  // если не выбран офис
-  // если не заполнено одно из полей
-  // если неправильный email
+  const initialData = {
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  };
+  const [disabled, setDisabled] = useState(true);
+  const [formData, setFormData] = useState(initialData);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    console.log(formData);
+  };
+  // validation
+  // if no office is chosen
+  // if some field isn't filled
+  // if wrong email
 
   return (
     <form
@@ -32,21 +48,29 @@ const Form = ({ btnText, btnColor, options, selectPlaceholder }) => {
         className={styles.form__input}
         name="name"
         placeholder={t("form_name_placeholder")}
+        value={formData.name}
+        onChange={handleChange}
       />
       <input
         className={styles.form__input}
         name="email"
         placeholder={t("form_email_placeholder")}
+        value={formData.email}
+        onChange={handleChange}
       />
       <input
         className={styles.form__input}
         name="phone"
         placeholder={t("form_phone_placeholder")}
+        value={formData.phone}
+        onChange={handleChange}
       />
       <textarea
         className={`${styles.form__input} ${styles.form__textarea}`}
         name="message"
         placeholder={t("form_message_placeholder")}
+        value={formData.message}
+        onChange={handleChange}
       />
 
       {/* // put thank u page */}
@@ -57,10 +81,20 @@ const Form = ({ btnText, btnColor, options, selectPlaceholder }) => {
         name="_autoresponse"
         value="Thank you for choosing our company! We got your request"
       ></input>
-      <Link target="_blank" to="/privacy_policy">
-        {t("privacy_policy")}
-      </Link>
-      <ContactBtn text={btnText} options={options} onClick={TestButton} />
+      <label>
+        <input type="checkbox" />
+        {t("i_agree_with")}
+        <Link className="link" target="_blank" to="/privacy_policy">
+          {t("privacy_policy")}
+        </Link>
+      </label>
+
+      <ContactBtn
+        disabledMode={disabled}
+        text={btnText}
+        options={options}
+        onClick={TestButton}
+      />
     </form>
   );
 };
