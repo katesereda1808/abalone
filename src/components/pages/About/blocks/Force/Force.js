42cc305e84918d4f4d2976932b366d69f6c8e680
katesereda1808 <katesereda22@gmail.com>
2023-04-29 02:11:28 +0200

    feat: add eng version of legal notice

--- src/components/pages/About/blocks/Force/Force.js
+++ src/components/pages/About/blocks/Force/Force.js
@@ -8,6 +8,12 @@ const Force = () => {
         <div className={styles.content}>
           <h2 className={styles.heading}>{t("our_force")}</h2>
           <p className={styles.text}>{t("why_team")}</p>
+          <p className={styles.source_attribute}>
+            Image by{" "}
+            <a href="https://www.freepik.com/premium-photo/man-planning-executive-workplace-businesswoman_1083176.htm">
+              Freepik
+            </a>
+          </p>
         </div>
       </div>
     </div>
--- src/components/pages/Home/blocks/Introduction/Introduction.js
+++ src/components/pages/Home/blocks/Introduction/Introduction.js
@@ -9,6 +9,12 @@ const Introduction = () => {
         {/* <h1 className={styles.title}>abalone</h1> */}
         <img className={styles.logo_text} src={logo_text} alt="abalone" />
         <div className={styles.details}>{t("home_description")}</div>
+        <p className={styles.source_attribute}>
+          Image by{" "}
+          <a href="https://www.freepik.com/free-photo/side-view-cropped-unrecognizable-business-people-working-common-desk_5839469.htm">
+            Freepik
+          </a>
+        </p>
       </div>
     </div>
   );
